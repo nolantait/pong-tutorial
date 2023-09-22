b4fbc065c64084776ea5708beb97e38256caffc4
Nolan J Tait <nolanjtait@gmail.com>
2023-09-22 15:36:40 -0700

    Adds basic working collisions

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,119 @@
 use bevy::prelude::*;
-use bevy::sprite::MaterialMesh2dBundle;
+use bevy::sprite::{
+    MaterialMesh2dBundle,
+    collide_aabb::{collide, Collision}
+};
+
 
 #[derive(Component)]
 struct Ball;
 
+#[derive(Bundle)]
+struct BallBundle {
+    ball: Ball,
+    shape: Shape,
+    velocity: Velocity,
+    position: Position
+}
+
+impl BallBundle {
+    fn new(x: f32, y: f32) -> Self {
+        Self {
+            ball: Ball,
+            shape: Shape(Vec2::new(5., 5.)),
+            velocity: Velocity(Vec2::new(x, y)),
+            position: Position(Vec2::new(0., 0.)),
+        }
+    }
+}
+
 #[derive(Component)]
 struct Paddle;
+
+#[derive(Bundle)]
+struct PaddleBundle {
+    paddle: Paddle,
+    shape: Shape,
+    position: Position,
+    velocity: Velocity
+}
+
+impl PaddleBundle {
+    fn new(x: f32, y: f32) -> Self {
+        Self {
+            paddle: Paddle,
+            shape: Shape(Vec2::new(5., 50.)),
+            position: Position(Vec2::new(x, y)),
+            velocity: Velocity(Vec2::new(0., 0.)),
+        }
+    }
+}
+
+#[derive(Component)]
+struct Position(Vec2);
 
 #[derive(Component)]
 struct Velocity(Vec2);
 
-fn move_ball(mut ball: Query<(&mut Transform, &Velocity), With<Ball>>) {
-    if let Ok((mut transform, velocity)) = ball.get_single_mut() {
-        transform.translation += velocity.0.extend(0.);
+#[derive(Component)]
+struct Shape(Vec2);
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (spawn_ball, spawn_camera, spawn_paddles))
+        .add_systems(Update, (
+            move_ball,
+            project_positions.after(move_ball),
+            handle_collisions.after(move_ball),
+        ))
+        .run();
+}
+
+fn project_positions(mut ball: Query<(&mut Transform, &Position)>) {
+    for (mut transform, position) in &mut ball {
+        transform.translation = position.0.extend(0.);
     }
 }
 
-fn collide_with_paddle(
-    mut ball: Query<(&mut Velocity, &Transform), With<Ball>>,
-    paddles: Query<(&Transform, Mesh), With<Paddle>>,
+fn move_ball(mut ball: Query<(&mut Position, &Velocity), With<Ball>>) {
+    if let Ok((mut position, velocity)) = ball.get_single_mut() {
+        position.0 += velocity.0;
+    }
+}
+
+fn handle_collisions(
+    mut ball: Query<(&mut Velocity, &Position, &Shape), With<Ball>>,
+    other_things: Query<(&Position, &Shape), Without<Ball>>,
 ) {
+    if let Ok((mut ball_velocity, ball_position, ball_shape)) = ball.get_single_mut() {
+        for (position, shape) in &other_things {
+            if let Some(collision) = collide(
+                ball_position.0.extend(0.),
+                ball_shape.0,
+                position.0.extend(0.), 
+                shape.0
+            ) {
+                match collision {
+                    Collision::Left => {
+                        ball_velocity.0.x *= -1.;
+                    }
+                    Collision::Right => {
+                        ball_velocity.0.x *= -1.;
+                    }
+                    Collision::Top => {
+                        ball_velocity.0.y *= -1.;
+                    }
+                    Collision::Bottom => {
+                        ball_velocity.0.y *= -1.;
+                    }
+                    Collision::Inside => {
+                        // Do nothing
+                    }
+                }
+            }
+        }
+    }
 }
 
 fn spawn_paddles(
@@ -32,11 +126,10 @@ fn spawn_paddles(
     let material = materials.add(Color::rgb(0., 1., 0.).into());
 
     commands.spawn((
-        Paddle,
+        PaddleBundle::new(100., 0.),
         MaterialMesh2dBundle {
             mesh: mesh.into(),
             material: material.into(),
-            transform: Transform::from_xyz(100., 0., 0.),
             ..default()
         },
     ));
@@ -52,12 +145,10 @@ fn spawn_ball(
     let material = materials.add(Color::rgb(1., 0., 0.).into());
 
     commands.spawn((
-        Ball,
-        Velocity(Vec2::new(1., 0.)),
+        BallBundle::new(1., 0.),
         MaterialMesh2dBundle {
             mesh: mesh.into(),
             material: material.into(),
-            transform: Transform::from_xyz(0., 0., 0.),
             ..default()
         },
     ));
@@ -65,12 +156,4 @@ fn spawn_ball(
 
 fn spawn_camera(mut commands: Commands) {
     commands.spawn_empty().insert(Camera2dBundle::default());
-}
-
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (spawn_ball, spawn_camera, spawn_paddles))
-        .add_systems(Update, move_ball)
-        .run();
 }
